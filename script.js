f997d5d24e27db4ac16dd5fd780d03b8c80bbdc4
Hariprasath1996 <hasathphy96@gmail.com>
2023-06-01 22:23:33 +0530

    practicing js

---
+++ script.js
@@ -0,0 +1,93 @@
+
+// variables
+
+// const firstName='hari'
+// let city='erode'
+// console.log(city);
+// city='chennimalai'
+
+// console.log(city)
+// console.log(firstName)
+
+// const secondName='prasath'
+// console.log(secondName);
+// secondName='hari'
+// console.log(secondName);
+
+// const isRaining = false
+// console.log(isRaining)
+
+
+
+
+// comparison operator
+
+
+
+// console.log(
+//   3==4
+// );
+// console.log(9==3**2);
+
+//  branching --if , else if
+
+// const randomNumber=55;
+
+// let userGuess=55
+ 
+// if (userGuess < randomNumber) {
+//   console.log('hi');
+// }
+// else if (userGuess > randomNumber){
+//   console.log('hello');
+// }
+// else {(userGuess=randomNumber)
+//   console.log('Welcome')
+// }
+
+// alert box
+
+
+// const weekday = prompt(`what day is today`)
+// console.log(weekday);
+
+
+
+// switch case
+
+// const weekDay=prompt(`what day is today?`)
+
+// switch (weekDay) {
+//   case 'Monday':
+//     console.log(`today is monday`);
+//     break;
+//     case 'tuesday':
+//     console.log(`today is tuesday`);
+//     case 'wednesday':
+//     console.log(`today is  wednesday`);
+//     case 'thursday':
+//     console.log(`today is thursday`);
+//     break;
+//     case 'friday':
+//     console.log(`today is friday`);
+//     case 'saturday':
+//     console.log(`today is saturday`);
+//     default:
+//     console.log(`Enter a correct weekday`);
+
+// }
+
+
+// Ternary operator
+
+// we can use aslo if and else if case here
+
+// const isRaining = false;
+
+// isRaining
+
+// ? console.log('a')
+// : console.log('b');
+
+
+
