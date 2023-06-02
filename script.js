9b9069aba913b7a57eb209e2f5f88983fada3098
Hariprasath1996 <hasathphy96@gmail.com>
2023-06-02 22:33:54 +0530

    js practising

--- script.js
+++ script.js
@@ -89,5 +89,207 @@
 // ? console.log('a')
 // : console.log('b');
 
+// operators
+
+// arithmatic , logic ,comparison , string concatination ,template literal
+
+
+// arithmatic operators
+
+// (+,-,*,/,**,%)
+// examples for Artithmatic operators
+// console.log(3+5)
+// console.log(3-5)
+// console.log(3*5)
+// console.log(3/5)
+// console.log(3**5)
+// console.log(3%5)
+
+
+
+
+// string concatination
+
+// add two string data types 
+
+// const firstName ='hari'
+// const secondName ='prasath'
+
+// const fullName = firstName +' '+secondName
+
+// console.log(fullName)
+
+
+
+
+// template literal
+
+// const a=5
+// const b=10
+
+// console.log(`${a+b}`)
+
+// const name=`hari`
+// const age=26
+
+// console.log(`i am ${name} and i am ${age} years old`);
+
+
+
+
+// comparison operator
+
+// ( ==,  !=  , <,  >,  <=,  >= )
+
+// console.log(`a`==5)
+// console.log(3==5);
+// console.log(3+5==5+3);
+// console.log(2+`apple`==2**`apple`)
+
+// const firstName=`hari`
+// const secondName='prasath'
+// const age=26
+
+// const fullName=  firstName + secondName 
+
+// console.log(fullName);
+
+// console.log(`my name is ${fullName} and i am ${age} years old`);
+
+// let a=5
+// let b=6
+// console.log(a==b);
+// console.log(a<b)
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+
+
+// logical operator
+
+
+// && (and) , || (or) , ! (not)
+
+
+// &&
+
+// its a boolen format. (i/p and o/p )
+
+// console.log(1>5 && 5>6)
+// console.log(1>0.5 && 5<6)
+// console.log(15==5 && 5==5)
+// console.log(3**2==9 && 2**2==4)
+
+// let a =5
+// let b=5
+//  let result= a+b
+//  console.log(a==b && a+b!=a);
+
+//  let a =14
+//  let b=24
+//  let result =a+b
+// console.log(a==b && a+b == b);
+
+
+
+// ||
+
+// let a=5
+// let b=3
+// console.log(a==b || b==a);
+// console.log(a!=b || b==a);
+// console.log(a!=b || b!=a);
+// console.log(a!=b || b>=a);
+// console.log(a<b || b>a);
+// console.log(a>b || b<a);
+
+
+// ! (negotion)
+// console.log(!(5==6))console.log(!(false));= true
+// console.log(!(6==6))=console.log(!(true));= false
+
+
+
+
+// Branching ---if , else if , else
+
+// const randomNumber=70
+
+// let userGuess=70
+
+// if (userGuess<randomNumber) {
+//   console.log(`low`);
+  
+// }
+// else if (userGuess>randomNumber) {
+//   console.log(`high`);
+
+// } 
+// else {
+// console.log(`you are correct`) 
+// }
+
+
+// const numberOfRestaurent=80
+
+// let userGuess=20
+
+// if(userGuess<numberOfRestaurent){
+//   console.log(`low`);
+// }
+// if (userGuess>numberOfRestaurent) {
+//   console.log(`high`);
+// } 
+// else {
+//   console.log(`you are correct`);
+// }
+
+
+
+// prompt
+
+// const weekDay= prompt(`what day is today ? `)
+
+// console.log(weekDay)
+
+
+// ternary operator
+
+// const isRaining= true
+
+// if (isRaining) {
+//   console.log(`take an umbrella`);
+// }
+// else  
+// {
+// console.log(  `Dont take an umbrella`);
+// }
+
+// isRaining
+// ? console.log(`take an umbrella`)
+// :console.log(`Dont take an umbrella`);
+
+
+
+// iterations (loops)
+
+// forloops
+
+
+// const maxVoters=50
+// let voterscount=1
+
+// for (let i = 0; i < maxVoters; i=i+1) {
+//   console.log( `voters counted ${i}`);
+// }
+
+// const totalBanana=50
+// let eaters=1
+// for (let i = 0; i <totalBanana; i=i+1) {
+// console.log(`eaters eating a banana ${i}`);
+  
+// }
+
 
 
